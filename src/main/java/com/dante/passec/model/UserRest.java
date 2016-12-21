a29b2fd49927c686e4ce3ed2fb21d124144bc0fd
Sergey Makarenko <sergey.king96@gmail.com>
2016-12-21 22:01:29 +0300

    Created UserRest model for entity! Tuned hibernate mapping. (reverted from commit 14506a9cdc2ded27d81a877303dd370e6de7b7df)

--- src/main/java/com/dante/passec/model/UserRest.java
+++ src/main/java/com/dante/passec/model/UserRest.java
@@ -1,7 +1,27 @@
 package com.dante.passec.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
 /**
+ * Model for entity user_rest
+ * @author Dante de Braso
+ * @version 1.0
  * Created by We on 21.12.2016.
  */
+@Entity(name = "user_rest")
 public class User {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    Long id;
+
+    @Column(name = "login", nullable = false, length = 30)
+    @Size(min = 6)
+    String login;
+
+    @Column(name = "password", nullable = false, length = 30)
+
 }
--- src/test/java/com/dante/passec/config/DabaBaseConfigForTest.java
+++
@@ -1,7 +0,0 @@
-package com.dante.passec.config;
-
-/**
- * Created by We on 21.12.2016.
- */
-public class DabaBaseConfigForTest {
-}
