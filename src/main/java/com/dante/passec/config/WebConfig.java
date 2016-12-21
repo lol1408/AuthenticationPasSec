51e7fee3bc08cf5f96f7399c916fb2873cd9a11a
Sergey Makarenko <sergey.king96@gmail.com>
2016-12-22 01:08:32 +0300

    second commit

--- src/main/java/com/dante/passec/config/WebConfig.java
+++ src/main/java/com/dante/passec/config/WebConfig.java
@@ -1,7 +1,19 @@
 package com.dante.passec.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
+ * Web config for RESTfull service
+ * @author Dante de Braso
+ * @version 1.0
  * Created by We on 22.12.2016.
  */
-public class WebConfig {
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.dante.passec")
+public class WebConfig{
+
+
 }
--- src/test/java/com/dante/passec/config/TestConfig.java
+++ src/test/java/com/dante/passec/config/TestConfig.java
@@ -1,7 +1,19 @@
 package com.dante.passec.config;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
 /**
  * Created by We on 22.12.2016.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class TestConfig {
+    @Test
+    public void whenSpringContextIsInstantiated_thenNoExceptions(){
+        // When
+    }
 }
