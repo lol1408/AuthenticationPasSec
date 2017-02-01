69ab237fa878fa6931a830f1c4813ad532ef3206
Makarenko Sergey <sergey.king96@gmail.com>
2017-02-01 22:52:08 +0200

    Add test for ResourceData

--- src/test/java/com/dante/passec/services/ResourceDataServiceTest.java
+++ src/test/java/com/dante/passec/services/ResourceDataServiceTest.java
@@ -3,9 +3,12 @@ package com.dante.passec.services;
 import com.dante.passec.configs.HibernateConfigT;
 import com.dante.passec.model.ResourceData;
 import com.dante.passec.model.UserRest;
+import com.dante.passec.utils.ResourceDataManager;
 import com.dante.passec.utils.UserRestManager;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -32,6 +35,8 @@ public class ResourceDataServiceTest extends Assert{
 
     @Resource
     private UserRestService userService;
+    @Resource
+    private ResourceDataService resourceDataService;
 
     List<UserRest> userRests;
     List<ResourceData> resourceDatas;
@@ -40,7 +45,33 @@ public class ResourceDataServiceTest extends Assert{
     public void setUp(){
         em = emf.createEntityManager();
         userRests = new ArrayList<>();
+        resourceDatas = new ArrayList<>();
         userRests.add(UserRestManager.createUser("hello world", "worldes"));
+        resourceDatas.add(ResourceDataManager.createResourceData("mama", "I love you"));
+    }
+
+    @Test
+    public void insertTest(){
+        userService.addUser(userRests.get(0));
+        resourceDatas.get(0).setUser(userService.userByLogin("hello world"));
+        System.out.println(resourceDatas.get(0));
+        resourceDataService.addResource(resourceDatas.get(0));
+    }
+    @Test
+    public void deleteTest(){
+        userService.addUser(userRests.get(0));
+        UserRest user = userService.userByLogin("hello world");
+        resourceDatas.get(0).setUser(user);
+        resourceDataService.addResource(resourceDatas.get(0));
+        List<ResourceData> resourcesByUser = resourceDataService.getResourcesByUser(user);
+        resourceDataService.deleteResource(resourcesByUser.get(0).getId());
+    }
+
+
+    @After
+    public void destroy(){
+        userRests = null;
+        resourceDatas = null;
     }
 
 }
