2ac1cf3d3714065529e084591666f2e05f8ece02
Makarenko Sergey <sergey.king96@gmail.com>
2017-03-23 20:25:08 +0200

    mini refactoring

--- src/test/java/com/dante/passec/config/TestHibernateConfig.java
+++ src/test/java/com/dante/passec/config/TestHibernateConfig.java
@@ -6,9 +6,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-/**
- * Created by sergey on 09.01.17.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {HibernateConfig.class, MainConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class TestHibernateConfig {
